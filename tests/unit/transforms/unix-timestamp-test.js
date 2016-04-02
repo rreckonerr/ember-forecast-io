8dcd5055dc73cfe45049ad42bd900bc47e8f0a9b
Moritz Lawitschka <me@moritzlawitschka.de>
2016-04-02 11:39:20 +0200

    Test Unix timestamp transform

--- tests/unit/transforms/unix-timestamp-test.js
+++ tests/unit/transforms/unix-timestamp-test.js
@@ -1,19 +1,42 @@
 /* jshint expr:true */
 import { expect } from 'chai';
+import { describe } from 'mocha';
 import { describeModule, it } from 'ember-mocha';
 
 describeModule(
-  'transform:unix-timestamp',
+  'transform:ember-forecast-io/unix-timestamp',
   'Unit | Transform | unix timestamp',
   {
     // Specify the other units that are required for this test.
     // needs: ['transform:foo']
   },
   function() {
-    // Replace this with your real tests.
-    it('exists', function() {
-      let transform = this.subject();
-      expect(transform).to.be.ok;
+    describe('deserialize', function() {
+      it('exists', function() {
+        let transform = this.subject();
+        expect(transform.deserialize).to.be.ok;
+      });
+
+      it('transforms a Date into a Unix timestamp', function() {
+        let transform = this.subject();
+        let date = new Date();
+        let unixTimestamp = date.getTime() / 1000;
+        expect(transform.deserialize(unixTimestamp)).to.be.eql(date);
+      });
+    });
+
+    describe('serialize', function() {
+      it('exists', function() {
+        let transform = this.subject();
+        expect(transform.serialize).to.be.ok;
+      });
+
+      it('transforms a Unix timestamp into a date', function() {
+        let transform = this.subject();
+        let date = new Date();
+        let unixTimestamp = date.getTime() / 1000;
+        expect(transform.serialize(date)).to.be.eq(unixTimestamp);
+      });
     });
   }
 );
